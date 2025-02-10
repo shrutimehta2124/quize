cdaa30995d188775ca5196bb072a5f495d5e6b3a
shrutimehta2124 <shrutimehta.sarvadhi@gmail.com>
2025-02-10 10:18:23 +0530

    crate basic 3 file

---
+++ script.js
@@ -0,0 +1,14 @@
+const question = [
+    {
+        question:"What does HTML stand for?",
+        Option:["Hyper Transfer Markup Language","Hyper Text Markup Language",
+            "High Text Machine Language","Hyperlink and Text Markup Language"],
+        answer:"Hyper Text Markup Language"
+    },
+    {
+        question:"Which HTML tag is used to create a hyperlink?",
+        Option:["<link>","<a>","<href>","<nav>"],
+        answer:"<nav>"
+    },
+    
+];
